aaa6413c5ef95d8fc3a31f9566fc1df80dadc261
Mohan-kumar24 <mohankumarbc2404@gmail.com>
2024-10-29 10:47:19 +0530

    Project Started

---
+++ src/main/java/com/springboot/pharmassist/SpringBootPharmassistApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.pharmassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPharmassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPharmassistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/pharmassist/SpringBootPharmassistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.pharmassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPharmassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
